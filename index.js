e5e49dfde959a8a3ad359b4e53d7dc0e9bcd98a6
Jesse Taube <Mr.Bossman075@gmail.com>
2022-08-25 14:55:37 -0400

    slightly better front end and use recursive dir for gallary

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const app = require('express')();
 const fs = require('fs');
+const path = require('path');
 const request = require("request");
 const global_settings = JSON.parse(fs.readFileSync('gallery-settings.json'));
 
 var sitemap = ["", "robots.txt", "favicon.ico", "images.json", "sitemap.xml", "sizes.json", "featured.json", "main.css", "generator.js"];
 
-fs.readdirSync('./gallery/').filter(image => !global_settings["exclude"].includes(image)).forEach(file => {
+const readdirRec = (prefix, dir) => {
+	if (dir === undefined) dir = "";
+	let res = [];
+	fs.readdirSync(path.resolve(prefix + dir)).forEach(file => {
+		file = dir + file;
+		const stat = fs.statSync(path.resolve(prefix + file));
+		if (stat && stat.isDirectory())
+			res = res.concat(readdirRec(prefix, file + '/'));
+		else
+			res.push(file);
+	});
+	return res;
+}
+
+const genSiteMap = () => {
+	const lastmod = new Date(Date.now()).toISOString().split("T")[0];
+	let response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
+	sitemap.forEach(function (URLs) {
+		response += `<url>\n<loc>https://${global_settings["site-name"]}/${URLs}</loc>\n`;
+		response += "<lastmod>" + lastmod + "</lastmod>\n</url>\n";
+	});
+	response += "</urlset>";
+	app.get("/sitemap.xml", (req, res) => {
+		res.send(response);
+		res.status(200).end();
+	});
+}
+
+readdirRec('./gallery/').filter(image => !global_settings["exclude"].includes(image)).forEach(file => {
 	app.get('/image/' + file, (req, res) => {
 		res.sendFile('./gallery/' + file, { root: __dirname });
 	});
@@ -13,11 +42,7 @@ fs.readdirSync('./gallery/').filter(image => !global_settings["exclude"].include
 });
 
 app.get('/images.json', (req, res) => {
-	res.json(fs.readdirSync('./gallery/').filter(image => !global_settings["exclude"].includes(image)));
-});
-
-app.get('/images.json', (req, res) => {
-	res.json(fs.readdirSync('./gallery/').filter(image => !global_settings["exclude"].includes(image)));
+	res.json(readdirRec('./gallery/').filter(image => !global_settings["exclude"].includes(image)));
 });
 
 app.get('/sizes.json', (req, res) => {
@@ -47,20 +72,6 @@ app.get("/robots.txt", (req, res) => {
 	res.send(`User-agent: *\nAllow: /\nSitemap: https://${global_settings["site-name"]}/sitemap.xml`);
 	res.status(200).end();
 });
-
-function genSiteMap() {
-	const lastmod = new Date(Date.now()).toISOString().split("T")[0];
-	let response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
-	sitemap.forEach(function (URLs) {
-		response += `<url>\n<loc>https://${global_settings["site-name"]}/${URLs}</loc>\n`;
-		response += "<lastmod>" + lastmod + "</lastmod>\n</url>\n";
-	});
-	response += "</urlset>";
-	app.get("/sitemap.xml", (req, res) => {
-		res.send(response);
-		res.status(200).end();
-	});
-}
 
 genSiteMap();
 
